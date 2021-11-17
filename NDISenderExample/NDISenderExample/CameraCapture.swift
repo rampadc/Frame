f98a74bfc2d77916e9f452c82e544838a8a4e679
Cong <rampadc@users.noreply.github.com>
2021-11-18 09:21:05 +1100

    Updated library includes to NDI v5 advanced

--- NDISenderExample/NDISenderExample/CameraCapture.swift
+++ NDISenderExample/NDISenderExample/CameraCapture.swift
@@ -15,7 +15,7 @@ class CameraCapture: NSObject {
   
   private let output = AVCaptureVideoDataOutput()
   
-  private var isUsingFilters = true
+  private var isUsingFilters = false
     
   init(cameraPosition: AVCaptureDevice.Position, processingCallback: @escaping ProcessingCallback) {
     self.cameraPosition = cameraPosition
@@ -62,7 +62,7 @@ class CameraCapture: NSObject {
       session.addInput(input)
     }
     
-    output.videoSettings = [kCVPixelBufferPixelFormatTypeKey :  kCVPixelFormatType_32BGRA] as [String : Any]
+    output.videoSettings = [kCVPixelBufferPixelFormatTypeKey : kCVPixelFormatType_32BGRA] as [String : Any]
     output.alwaysDiscardsLateVideoFrames = true
     output.setSampleBufferDelegate(self, queue: sampleBufferQueue)
     
