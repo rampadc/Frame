4ae2d4ccec0a6e67299b6f2f6ffff9c4058a7976
Cong Nguyen <rampadc@users.noreply.github.com>
2022-04-06 03:34:00 +1000

    Added OSLog to PixelUtils

--- NDISenderExample/NDISenderExample/PixelUtils.swift
+++ NDISenderExample/NDISenderExample/PixelUtils.swift
@@ -6,10 +6,14 @@
 //
 
 import Foundation
+import os
 
-class PixelUtils { 
+class PixelUtils {
+  private let logger = Logger(subsystem: Config.shared.subsystem, category: "CameraCapture")
+  static let instance = PixelUtils()
+  
   // MARK: Create a brand new SampleBuffer from a CVPixelBuffer
-  static func createSampleBufferFrom(pixelBuffer: CVPixelBuffer) -> CMSampleBuffer? {
+  func createSampleBufferFrom(pixelBuffer: CVPixelBuffer) -> CMSampleBuffer? {
     var sampleBuffer: CMSampleBuffer?
     
     var timimgInfo  = CMSampleTimingInfo()
@@ -26,62 +30,64 @@ class PixelUtils {
     
     // Print out errors
     if osStatus == kCMSampleBufferError_AllocationFailed {
-      print("osStatus == kCMSampleBufferError_AllocationFailed")
+      logger.error("Cannot create sample buffer. Error: kCMSampleBufferError_AllocationFailed")
     }
     if osStatus == kCMSampleBufferError_RequiredParameterMissing {
-      print("osStatus == kCMSampleBufferError_RequiredParameterMissing")
+      logger.error("Cannot create sample buffer. Error: kCMSampleBufferError_RequiredParameterMissing")
     }
     if osStatus == kCMSampleBufferError_AlreadyHasDataBuffer {
-      print("osStatus == kCMSampleBufferError_AlreadyHasDataBuffer")
+      logger.error("Cannot create sample buffer. Error: kCMSampleBufferError_AlreadyHasDataBuffer")
     }
     if osStatus == kCMSampleBufferError_BufferNotReady {
-      print("osStatus == kCMSampleBufferError_BufferNotReady")
+      logger.error("Cannot create sample buffer. Error: kCMSampleBufferError_BufferNotReady")
     }
     if osStatus == kCMSampleBufferError_SampleIndexOutOfRange {
-      print("osStatus == kCMSampleBufferError_SampleIndexOutOfRange")
+      logger.error("Cannot create sample buffer. Error: kCMSampleBufferError_SampleIndexOutOfRange")
     }
     if osStatus == kCMSampleBufferError_BufferHasNoSampleSizes {
-      print("osStatus == kCMSampleBufferError_BufferHasNoSampleSizes")
+      logger.error("Cannot create sample buffer. Error: kCMSampleBufferError_BufferHasNoSampleSizes")
     }
     if osStatus == kCMSampleBufferError_BufferHasNoSampleTimingInfo {
-      print("osStatus == kCMSampleBufferError_BufferHasNoSampleTimingInfo")
+      logger.error("Cannot create sample buffer. Error: kCMSampleBufferError_BufferHasNoSampleTimingInfo")
     }
     if osStatus == kCMSampleBufferError_ArrayTooSmall {
-      print("osStatus == kCMSampleBufferError_ArrayTooSmall")
+      logger.error("Cannot create sample buffer. Error: kCMSampleBufferError_ArrayTooSmall")
     }
     if osStatus == kCMSampleBufferError_InvalidEntryCount {
-      print("osStatus == kCMSampleBufferError_InvalidEntryCount")
+      logger.error("Cannot create sample buffer. Error: kCMSampleBufferError_InvalidEntryCount")
     }
     if osStatus == kCMSampleBufferError_CannotSubdivide {
-      print("osStatus == kCMSampleBufferError_CannotSubdivide")
+      logger.error("Cannot create sample buffer. Error: kCMSampleBufferError_CannotSubdivide")
     }
     if osStatus == kCMSampleBufferError_SampleTimingInfoInvalid {
-      print("osStatus == kCMSampleBufferError_SampleTimingInfoInvalid")
+      logger.error("Cannot create sample buffer. Error: kCMSampleBufferError_SampleTimingInfoInvalid")
     }
     if osStatus == kCMSampleBufferError_InvalidMediaTypeForOperation {
-      print("osStatus == kCMSampleBufferError_InvalidMediaTypeForOperation")
+      logger.error("Cannot create sample buffer. Error: kCMSampleBufferError_InvalidMediaTypeForOperation")
     }
     if osStatus == kCMSampleBufferError_InvalidSampleData {
-      print("osStatus == kCMSampleBufferError_InvalidSampleData")
+      logger.error("Cannot create sample buffer. Error: kCMSampleBufferError_InvalidSampleData")
     }
     if osStatus == kCMSampleBufferError_InvalidMediaFormat {
-      print("osStatus == kCMSampleBufferError_InvalidMediaFormat")
+      logger.error("Cannot create sample buffer. Error: kCMSampleBufferError_InvalidMediaFormat")
     }
     if osStatus == kCMSampleBufferError_Invalidated {
-      print("osStatus == kCMSampleBufferError_Invalidated")
+      logger.error("Cannot create sample buffer. Error: kCMSampleBufferError_Invalidated")
     }
     if osStatus == kCMSampleBufferError_DataFailed {
-      print("osStatus == kCMSampleBufferError_DataFailed")
+      logger.error("Cannot create sample buffer. Error: kCMSampleBufferError_DataFailed")
     }
     if osStatus == kCMSampleBufferError_DataCanceled {
-      print("osStatus == kCMSampleBufferError_DataCanceled")
+      logger.error("Cannot create sample buffer. Error: kCMSampleBufferError_DataCanceled")
     }
     
     guard let buffer = sampleBuffer else {
-      print("Cannot create sample buffer")
+      logger.error("Cannot create sample buffer. Error: UNKNOWN")
       return nil
     }
     
     return buffer
   }
+  
+  private init() {}
 }
